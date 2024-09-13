4380c91d91ecfb4cc0523dd7f4d1f4ce07785e1d
sijosaji <sijosaji49@gmail.com>
2024-09-13 09:13:00 +0530

    Add tests

--- src/test/java/com/mongodbdemo/ratelimiter/RatelimiterApplicationTests.java
+++ src/test/java/com/mongodbdemo/ratelimiter/RatelimiterApplicationTests.java
@@ -1,13 +1,25 @@
 package com.mongodbdemo.ratelimiter;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.boot.SpringApplication;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.mockStatic;
 
 @SpringBootTest
 class RatelimiterApplicationTests {
 
 	@Test
 	void contextLoads() {
+		try (var mockedSpringApplication = mockStatic(SpringApplication.class)) {
+			mockedSpringApplication.when(() -> SpringApplication.run(any(Class.class), any(String[].class)))
+					.thenReturn(null);
+
+			RatelimiterApplication.main(new String[]{});
+
+			mockedSpringApplication.verify(() -> SpringApplication.run(RatelimiterApplication.class, new String[]{}));
+		}
 	}
 
 }
